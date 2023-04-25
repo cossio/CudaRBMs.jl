e29bf037fa6ff64ff924fe8be0d28e698e61ab87
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-04-25 16:40:26 +0200

    v1.3.1 fix ∂RBM

--- src/CudaRBMs.jl
+++ src/CudaRBMs.jl
@@ -11,8 +11,8 @@ cpu(x::AbstractArray) = adapt(Array, x)
 gpu(rbm::RBM) = RBM(gpu(rbm.visible), gpu(rbm.hidden), gpu(rbm.w))
 cpu(rbm::RBM) = RBM(cpu(rbm.visible), cpu(rbm.hidden), cpu(rbm.w))
 
-gpu(∂::∂RBM) = RBM(gpu(∂.visible), gpu(∂.hidden), gpu(∂.w))
-cpu(∂::∂RBM) = RBM(cpu(∂.visible), cpu(∂.hidden), cpu(∂.w))
+gpu(∂::∂RBM) = ∂RBM(gpu(∂.visible), gpu(∂.hidden), gpu(∂.w))
+cpu(∂::∂RBM) = ∂RBM(cpu(∂.visible), cpu(∂.hidden), cpu(∂.w))
 
 gpu(layer::Binary) = Binary(gpu(layer.par))
 cpu(layer::Binary) = Binary(cpu(layer.par))
